459ad919cdf7db41f760d05940e373d32202aea7
MuhamedAhmed011 <aklmuhamedahmed@gmail.com>
2026-04-15 13:53:44 +0200

    Question01

--- LinQApp1/Program.cs
+++ LinQApp1/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            //1) Get all products from the "Seafood" category.Print each product's name and price.
+
+            //var result = ProductList.Where(p => p.Category == "Seafood");
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.ProductName + " - " + item.UnitPrice);
+            //} 
+            #endregion
+
+
+
+
         }
     }
 }
