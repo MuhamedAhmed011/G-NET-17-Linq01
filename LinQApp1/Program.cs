94be63954918fa1b2b056daff4d4bb987078731c
MuhamedAhmed011 <aklmuhamedahmed@gmail.com>
2026-04-15 13:57:25 +0200

    Question04

--- LinQApp1/Program.cs
+++ LinQApp1/Program.cs
@@ -47,13 +47,17 @@ namespace LinQApp1
             #endregion
 
 
-            4) Get all products where UnitPrice is between 10 and 30.
-var result = ProductList.Where(p => p.UnitPrice >= 10 && p.UnitPrice <= 30);
+            #region Question04
+            //4) Get all products where UnitPrice is between 10 and 30.
 
-            foreach (var item in result)
-            {
-                Console.WriteLine(item.ProductName + " - " + item.UnitPrice);
-            }
+            //var result = ProductList.Where(p => p.UnitPrice >= 10 && p.UnitPrice <= 30);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item.ProductName + " - " + item.UnitPrice);
+            //} 
+            #endregion
+
             5) Get all products that are in stock(UnitsInStock > 0) and belong to the "Condiments" category.
             var result = ProductList.Where(p => p.UnitsInStock > 0 && p.Category == "Condiments");
 
